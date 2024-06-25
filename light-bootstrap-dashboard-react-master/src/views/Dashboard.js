342e7f03fbc33530a82aa2d5c87a7abbf2847176
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2024-06-25 17:45:08 +0100

    Dashboard atualização

--- light-bootstrap-dashboard-react-master/src/views/Dashboard.js
+++ light-bootstrap-dashboard-react-master/src/views/Dashboard.js
@@ -13,16 +13,15 @@ import {
   Tooltip,
 } from "react-bootstrap";
 
-
 function Dashboard() {
   const [accountValue, setAccountValue] = useState(0);
   const [inputValue, setInputValue] = useState(0);
   const [showForm, setShowForm] = useState(false);
-  const [category, setCategory] = useState('');
-  const [transactionType, setTransactionType] = useState('');
+  const [category, setCategory] = useState("");
+  const [transactionType, setTransactionType] = useState("");
 
-  const ganhoCategories = ['Salário', 'Presentes', 'Investimentos'];
-  const gastoCategories = ['Saúde', 'Lazer', 'Casa', 'Educação', 'Presentes'];
+  const ganhoCategories = ["Salário", "Presentes", "Investimentos"];
+  const gastoCategories = ["Saúde", "Lazer", "Casa", "Educação", "Presentes"];
 
   const [rendasDataGanho, setRendasDataGanho] = useState({
     labels: ganhoCategories,
@@ -56,47 +55,49 @@ function Dashboard() {
 
   const handleTransactionTypeChange = (event) => {
     setTransactionType(event.target.value);
-    setCategory('');
+    setCategory("");
   };
 
   const handleConfirm = () => {
-    if (transactionType === 'ganho') {
+    if (transactionType === "ganho") {
       setAccountValue(accountValue + inputValue);
-    } else if (transactionType === 'gasto') {
+    } else if (transactionType === "gasto") {
       setAccountValue(accountValue - inputValue);
     }
 
     const initialColors = [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 255, 255, 0.2)',
-      'rgba(0, 0, 0, 0.2)',
-      'rgba(255, 255, 255, 0.2)'
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+      "rgba(255, 255, 255, 0.2)",
+      "rgba(0, 0, 0, 0.2)",
+      "rgba(255, 255, 255, 0.2)",
     ];
 
-    if (transactionType === 'ganho') {
+    if (transactionType === "ganho") {
       setRendasDataGanho((prevData) => {
         const newData = { ...prevData };
         const index = newData.labels.indexOf(category);
         if (index !== -1) {
           newData.datasets[0].data[index] += inputValue;
           newData.datasets[0].backgroundColor[index] = initialColors[index];
-          newData.datasets[0].hoverBackgroundColor[index] = initialColors[index];
+          newData.datasets[0].hoverBackgroundColor[index] =
+            initialColors[index];
         }
         return newData;
       });
-    } else if (transactionType === 'gasto') {
+    } else if (transactionType === "gasto") {
       setRendasDataGasto((prevData) => {
         const newData = { ...prevData };
         const index = newData.labels.indexOf(category);
         if (index !== -1) {
           newData.datasets[0].data[index] += inputValue;
           newData.datasets[0].backgroundColor[index] = initialColors[index];
-          newData.datasets[0].hoverBackgroundColor[index] = initialColors[index];
+          newData.datasets[0].hoverBackgroundColor[index] =
+            initialColors[index];
         }
         return newData;
       });
@@ -108,40 +109,6 @@ function Dashboard() {
   return (
     <Container fluid>
       <Row>
-      <Col lg="6">
-          <h2 style={{ color: "white", marginBottom: "20px" }}>
-            <span style={{ fontSize: "2em" }}>{accountValue}</span>
-            <span style={{ fontSize: "0.5em" }}> EUR</span>
-          </h2>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            className="input"
-          />
-          <div className="button" onClick={() => setShowForm(true)}>+</div>
-          <div className={`form ${showForm ? 'show' : ''}`}>
-            <select className='dropdown' value={transactionType} onChange={handleTransactionTypeChange}>
-              <option value="">Selecione o tipo de transação</option>
-              <option value="ganho">Ganho</option>
-              <option value="gasto">Gasto</option>
-            </select>
-            <select className='dropdown' value={category} onChange={handleCategoryChange}>
-              <option value="">Selecione uma categoria</option>
-              {transactionType === 'ganho' && ganhoCategories.map((cat) => (
-                <option key={cat} value={cat}>{cat}</option>
-              ))}
-              {transactionType === 'gasto' && gastoCategories.map((cat) => (
-                <option key={cat} value={cat}>{cat}</option>
-              ))}
-            </select>
-            <button type="button" className="btn btn-outline-success" onClick={handleConfirm}>Confirmar</button>
-          </div>
-        </Col>
-        <Col lg="6">
-          {transactionType === 'ganho' && <Doughnut data={rendasDataGanho} />}
-          {transactionType === 'gasto' && <Doughnut data={rendasDataGasto} />}
-        </Col>
         <Col lg="3" sm="6">
           <Card className="card-stats">
             <Card.Body>
@@ -153,7 +120,7 @@ function Dashboard() {
                 </Col>
                 <Col xs="7">
                   <div className="numbers">
-                    <p className="card-category">Revenue</p>
+                    <p className="card-category">Valor da conta</p>
                     <Card.Title as="h4">{accountValue} EUR</Card.Title>
                   </div>
                 </Col>
@@ -169,77 +136,60 @@ function Dashboard() {
           </Card>
         </Col>
       </Row>
-
-
       <Row>
-        <Col md="6">
-          <Card className="card-tasks">
-            <Card.Header>
-              <Card.Title as="h4">Tasks</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <div className="table-full-width">
-                <Table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <Form.Check className="mb-1 pl-0">
-                          <Form.Check.Label>
-                            <Form.Check.Input
-                              defaultValue=""
-                              type="checkbox"
-                            />
-                            <span className="form-check-sign"></span>
-                          </Form.Check.Label>
-                        </Form.Check>
-                      </td>
-                      <td>
-                        Sign contract for "What are conference organizers
-                        afraid of?"
-                      </td>
-                      <td className="td-actions text-right">
-                        <OverlayTrigger
-                          overlay={
-                            <Tooltip id="tooltip-488980961">
-                              Edit Task
-                            </Tooltip>
-                          }
-                        >
-                          <Button
-                            className="btn-simple btn-link p-1"
-                            type="button"
-                            variant="info"
-                          >
-                            <i className="fas fa-edit"></i>
-                          </Button>
-                        </OverlayTrigger>
-                        <OverlayTrigger
-                          overlay={
-                            <Tooltip id="tooltip-506045838">Remove</Tooltip>
-                          }
-                        >
-                          <Button
-                            className="btn-simple btn-link p-1"
-                            type="button"
-                            variant="danger"
-                          >
-                            <i className="fas fa-times"></i>
-                          </Button>
-                        </OverlayTrigger>
-                      </td>
-                    </tr>
-                    
-                  </tbody>
-                </Table>
-              </div>
-            </Card.Body>
-            <Card.Footer>
-              <hr />
-            </Card.Footer>
-          </Card>
+        <Col>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            className="input"
+          />
+          <div className="button" onClick={() => setShowForm(true)}>
+            +
+          </div>
+          <div className={`form ${showForm ? "show" : ""}`}>
+            <select
+              className="dropdown"
+              value={transactionType}
+              onChange={handleTransactionTypeChange}
+            >
+              <option value="">Selecione o tipo de transação</option>
+              <option value="ganho">Ganho</option>
+              <option value="gasto">Gasto</option>
+            </select>
+            <select
+              className="dropdown"
+              value={category}
+              onChange={handleCategoryChange}
+            >
+              <option value="">Selecione uma categoria</option>
+              {transactionType === "ganho" &&
+                ganhoCategories.map((cat) => (
+                  <option key={cat} value={cat}>
+                    {cat}
+                  </option>
+                ))}
+              {transactionType === "gasto" &&
+                gastoCategories.map((cat) => (
+                  <option key={cat} value={cat}>
+                    {cat}
+                  </option>
+                ))}
+            </select>
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              onClick={handleConfirm}
+            >
+              Confirmar
+            </button>
+          </div>
+        </Col>
+        <Col lg="6" style={{marginRight: "200px"}}>
+          {transactionType === "ganho" && <Doughnut data={rendasDataGanho} />}
+          {transactionType === "gasto" && <Doughnut data={rendasDataGasto} />}
         </Col>
       </Row>
-
     </Container>
   );
 }
