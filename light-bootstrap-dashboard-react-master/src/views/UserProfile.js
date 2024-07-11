1b24689c3caafdd15f0834ba34aa51b97f87345e
Afonso Costa <afonso2004costa@gmail.com>
2024-07-11 22:28:04 +0100

    Possibilidade de update da conta do utilizador

--- light-bootstrap-dashboard-react-master/src/views/UserProfile.js
+++ light-bootstrap-dashboard-react-master/src/views/UserProfile.js
@@ -1,160 +1,147 @@
-import React from "react";
-// react-bootstrap components
-import {
-  Badge,
-  Button,
-  Card,
-  Form,
-  Navbar,
-  Nav,
-  Container,
-  Row,
-  Col
-} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Card, Form, Container, Row, Col } from "react-bootstrap";
 
 function User() {
+  const [user, setUser] = useState({
+    userName: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    userAutent: ""
+  });
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    const userFK = userData.userID;
+    try {
+      const response = await fetch(`https://localhost:7082/api/V1/Utilizadores?UserFK=${userFK}`);
+      const data = await response.json();
+      setUser({
+        userName: data.userName,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName
+      });
+    } catch (error) {
+      console.error("Erro ao buscar dados do usuário:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userData = JSON.parse(localStorage.getItem("user"));
+    const userFK = userData.userID;
+    const useremail = userData.email;
+    const userAutent = userData.userAutent;
+    
+    try {
+      const response = await fetch(`https://localhost:7082/api/V1/Utilizadores?UserFK=${userFK}&oldEmail=${userData.email}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          UserName: user.userName,
+          Email: user.email,
+          FirstName: user.firstName,
+          LastName: user.lastName,
+          UserAutent: userAutent
+        })
+      });
+  
+      if (response.ok) {
+        alert("Perfil atualizado com sucesso!");
+      } else {
+        const errorData = await response.json(); // Captura detalhes do erro se disponíveis
+        console.error("Erro ao atualizar perfil:", errorData);
+        alert("Erro ao atualizar perfil. Verifique o console para mais detalhes.");
+      }
+    } catch (error) {
+      console.error("Erro ao atualizar perfil:", error);
+      alert("Erro ao atualizar perfil. Verifique o console para mais detalhes.");
+    }
+  };
+
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col md="8">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Edit Profile</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Form>
-                  <Row>
-                    <Col className="px-1" md="6" style={{marginLeft:"12px"}}>
-                      <Form.Group>
-                        <label>Username</label>
-                        <Form.Control
-                          defaultValue=""
-                          placeholder="Username"
-                          type="text"
-                        
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="pl-1" md="6" style={{ marginLeft:"12px"}}>
-                      <Form.Group>
-                        <label htmlFor="exampleInputEmail1">
-                          Email address
-                        </label>
-                        <Form.Control
-                          placeholder="Email"
-                          type="email"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-1" md="6">
-                      <Form.Group>
-                        <label>First Name</label>
-                        <Form.Control
-                          defaultValue=""
-                          placeholder="First Name"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                    <Col className="pl-1" md="6">
-                      <Form.Group>
-                        <label>Last Name</label>
-                        <Form.Control
-                          defaultValue=""
-                          placeholder="Last Name"
-                          type="text"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  
-                  <Row>
-                    <Col md="12">
-                      <Form.Group>
-                        <label>About Me</label>
-                        <Form.Control
-                          cols="80"
-                          defaultValue=""
-                          placeholder="Here can be your description"
-                          rows="4"
-                          as="textarea"
-                        ></Form.Control>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Button
-                    className="btn-fill pull-right"
-                    type="submit"
-                    variant="info"
-                  >
-                    Update Profile
-                  </Button>
-                  <div className="clearfix"></div>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md="4">
-            <Card className="card-user">
-              <div className="card-image">
-                <img
-                  alt="..."
-                  src={require("assets/img/photo-1431578500526-4d9613015464.jpeg")}
-                ></img>
-              </div>
-              <Card.Body>
-                <div className="author">
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                    <img
-                      alt="..."
-                      className="avatar border-gray"
-                      src={require("assets/img/faces/face-3.jpg")}
-                    ></img>
-                    <h5 className="title">User</h5>
-                  </a>
-                  <p className="description"></p>
-                </div>
-                <p className="description text-center">
-                   <br></br>
-                   <br></br>
-                  
-                </p>
-              </Card.Body>
-              <hr></hr>
-              <div className="button-container mr-auto ml-auto">
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-facebook-square"></i>
+    <Container fluid>
+      <Row>
+        <Col md="8">
+          <Card>
+            <Card.Header>
+              <Card.Title as="h4">Edit Profile</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <Col className="px-1" md="6">
+                    <Form.Group>
+                      <Form.Label>Username</Form.Label>
+                      <Form.Control
+                        name="userName"
+                        value={user.userName}
+                        placeholder="Username"
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col className="px-1" md="6">
+                    <Form.Group>
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control
+                        name="email"
+                        value={user.email}
+                        placeholder="Email"
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="pr-1" md="6">
+                    <Form.Group>
+                      <Form.Label>First Name</Form.Label>
+                      <Form.Control
+                        name="firstName"
+                        value={user.firstName}
+                        placeholder="First Name"
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col className="pl-1" md="6">
+                    <Form.Group>
+                      <Form.Label>Last Name</Form.Label>
+                      <Form.Control
+                        name="lastName"
+                        value={user.lastName}
+                        placeholder="Last Name"
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Button className="btn-fill pull-right" type="submit" variant="info">
+                  Update Profile
                 </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-twitter"></i>
-                </Button>
-                <Button
-                  className="btn-simple btn-icon"
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                  variant="link"
-                >
-                  <i className="fab fa-google-plus-square"></i>
-                </Button>
-              </div>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+                <div className="clearfix" />
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
